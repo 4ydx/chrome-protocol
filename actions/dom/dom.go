33d8b300e9fe2dc38dcf7e83db161fed7308b2f5
4ydx <nathan@4ydx.com>
2018-08-17 05:08:42 +0900

    ActionCache replacing EventCache.

--- actions/dom/dom.go
+++ actions/dom/dom.go
@@ -9,20 +9,20 @@ import (
 )
 
 // GetDocument retrieves the root document.
-func GetDocument(id *cdp.ID, find string, timeout time.Duration, eventCache *cdp.EventCache, actionChan chan<- *cdp.Action, stepChan <-chan bool) *dom.GetDocumentReturns {
+func GetDocument(id *cdp.ID, find string, timeout time.Duration, actionCache *cdp.ActionCache, actionChan chan<- *cdp.Action, stepChan <-chan bool) *dom.GetDocumentReturns {
 	// Find nodes on the page if they exist.
 	a0 := cdp.NewAction([]cdp.Event{},
 		[]cdp.Step{
 			cdp.Step{Id: id.GetNext(), Method: dom.CommandGetDocument, Params: &dom.GetDocumentParams{}, Returns: &dom.GetDocumentReturns{}, Timeout: timeout},
 		})
-	a0.Run(eventCache, actionChan, stepChan)
+	a0.Run(actionCache, actionChan, stepChan)
 
 	return a0.Steps[0].Returns.(*dom.GetDocumentReturns)
 }
 
 // FindAll finds all nodes using XPath, CSS selector, or text.
-func FindAll(id *cdp.ID, find string, timeout time.Duration, eventCache *cdp.EventCache, actionChan chan<- *cdp.Action, stepChan <-chan bool) *dom.GetSearchResultsReturns {
-	doc := GetDocument(id, find, timeout, eventCache, actionChan, stepChan)
+func FindAll(id *cdp.ID, find string, timeout time.Duration, actionCache *cdp.ActionCache, actionChan chan<- *cdp.Action, stepChan <-chan bool) *dom.GetSearchResultsReturns {
+	doc := GetDocument(id, find, timeout, actionCache, actionChan, stepChan)
 	log.Printf("Doc %+v", doc)
 
 	// Find nodes on the page if they exist.
@@ -30,7 +30,7 @@ func FindAll(id *cdp.ID, find string, timeout time.Duration, eventCache *cdp.Eve
 		[]cdp.Step{
 			cdp.Step{Id: id.GetNext(), Method: dom.CommandPerformSearch, Params: &dom.PerformSearchParams{Query: find}, Returns: &dom.PerformSearchReturns{}, Timeout: timeout},
 		})
-	a0.Run(eventCache, actionChan, stepChan)
+	a0.Run(actionCache, actionChan, stepChan)
 
 	ret := a0.Steps[0].Returns.(*dom.PerformSearchReturns)
 	if ret.SearchID == "" || ret.ResultCount == 0 {
@@ -48,18 +48,18 @@ func FindAll(id *cdp.ID, find string, timeout time.Duration, eventCache *cdp.Eve
 		[]cdp.Step{
 			cdp.Step{Id: id.GetNext(), Method: dom.CommandGetSearchResults, Params: params, Returns: &dom.GetSearchResultsReturns{}, Timeout: timeout},
 		})
-	a1.Run(eventCache, actionChan, stepChan)
+	a1.Run(actionCache, actionChan, stepChan)
 
 	return a1.Steps[0].Returns.(*dom.GetSearchResultsReturns)
 }
 
 // Focus on the node identified by the given nodeId.
-func Focus(id *cdp.ID, nodeId cd.NodeID, timeout time.Duration, eventCache *cdp.EventCache, actionChan chan<- *cdp.Action, stepChan <-chan bool) *dom.FocusReturns {
+func Focus(id *cdp.ID, nodeId cd.NodeID, timeout time.Duration, actionCache *cdp.ActionCache, actionChan chan<- *cdp.Action, stepChan <-chan bool) *dom.FocusReturns {
 	a0 := cdp.NewAction([]cdp.Event{},
 		[]cdp.Step{
 			cdp.Step{Id: id.GetNext(), Method: dom.CommandFocus, Params: &dom.FocusParams{NodeID: nodeId}, Returns: &dom.FocusReturns{}, Timeout: timeout},
 		})
-	a0.Run(eventCache, actionChan, stepChan)
+	a0.Run(actionCache, actionChan, stepChan)
 
 	return a0.Steps[0].Returns.(*dom.FocusReturns)
 }
--- example/search/main.go
+++ example/search/main.go
@@ -10,24 +10,24 @@ import (
 )
 
 func main() {
-	eventCache, id, actionChan, stepChan, allComplete, shutdown := cdp.Start()
+	actionCache, id, actionChan, stepChan, allComplete, shutdown := cdp.Start()
 
 	// Enable all communication with chrome
 	a0 := ea.EnableDom(id, time.Second*2)
-	a0.Run(eventCache, actionChan, stepChan)
+	a0.Run(actionCache, actionChan, stepChan)
 	a1 := ea.EnablePage(id, time.Second*2)
-	a1.Run(eventCache, actionChan, stepChan)
+	a1.Run(actionCache, actionChan, stepChan)
 
 	// Navigate
 	a2 := pa.Navigate(id, "https://google.com", time.Second*5)
-	a2.Run(eventCache, actionChan, stepChan)
+	a2.Run(actionCache, actionChan, stepChan)
 
 	// FindAll objects matching the given string
-	res0 := da.FindAll(id, "#lst-ib", time.Second*5, eventCache, actionChan, stepChan)
+	res0 := da.FindAll(id, "#lst-ib", time.Second*5, actionCache, actionChan, stepChan)
 	if len(res0.NodeIds) == 0 {
 		panic("Expecting the search field to be present.")
 	}
-	res1 := da.Focus(id, res0.NodeIds[0], time.Second*5, eventCache, actionChan, stepChan)
+	res1 := da.Focus(id, res0.NodeIds[0], time.Second*5, actionCache, actionChan, stepChan)
 
 	log.Print("\n-- All completed --\n")
 	a0.Log()
