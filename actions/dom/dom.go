d467321a700a4ad03771275a6086e8d7e9cf07b6
4ydx <nathan@4ydx.com>
2018-08-16 23:50:48 +0900

    Trying to focus, but the returned node is "Not an element." Must have to take the node ID and inspect the different nodes until a proper input field is found...

--- actions/dom/dom.go
+++ actions/dom/dom.go
@@ -1,12 +1,14 @@
 package da
 
 import (
+	cd "github.com/4ydx/cdproto/cdp"
 	"github.com/4ydx/cdproto/dom"
 	"github.com/4ydx/chrome-protocol"
 	"log"
 	"time"
 )
 
+// GetDocument retrieves the root document.
 func GetDocument(id *cdp.ID, find string, timeout time.Duration, eventCache *cdp.EventCache, actionChan chan<- *cdp.Action, stepChan <-chan bool) *dom.GetDocumentReturns {
 	// Find nodes on the page if they exist.
 	a0 := cdp.NewAction([]cdp.Event{},
@@ -49,4 +51,15 @@ func FindAll(id *cdp.ID, find string, timeout time.Duration, eventCache *cdp.Eve
 	a1.Run(eventCache, actionChan, stepChan)
 
 	return a1.Steps[0].Returns.(*dom.GetSearchResultsReturns)
+}
+
+// Focus on the node identified by the given nodeId.
+func Focus(id *cdp.ID, nodeId cd.NodeID, timeout time.Duration, eventCache *cdp.EventCache, actionChan chan<- *cdp.Action, stepChan <-chan bool) *dom.FocusReturns {
+	a0 := cdp.NewAction([]cdp.Event{},
+		[]cdp.Step{
+			cdp.Step{Id: id.GetNext(), Method: dom.CommandFocus, Params: &dom.FocusParams{NodeID: nodeId}, Returns: &dom.FocusReturns{}, Timeout: timeout},
+		})
+	a0.Run(eventCache, actionChan, stepChan)
+
+	return a0.Steps[0].Returns.(*dom.FocusReturns)
 }
--- example/search/main.go
+++ example/search/main.go
@@ -24,12 +24,18 @@ func main() {
 
 	// FindAll objects matching the given string
 	res0 := da.FindAll(id, "#lst-ib", time.Second*5, eventCache, actionChan, stepChan)
+	if len(res0.NodeIds) == 0 {
+		panic("Expecting the search field to be present.")
+	}
+	res1 := da.Focus(id, res0.NodeIds[0], time.Second*5, eventCache, actionChan, stepChan)
 
 	log.Print("\n-- All completed --\n")
 	a0.Log()
 	a1.Log()
 	a2.Log()
 	log.Printf("res0 %+v\n", res0)
+	log.Printf("res1 %+v\n", res1)
 
+	time.Sleep(time.Second * 10)
 	cdp.Stop(allComplete, shutdown)
 }
