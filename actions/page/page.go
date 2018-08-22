17bf3c31c0c58471de06359c5665d22eba2c8825
4ydx <nathan@4ydx.com>
2018-08-23 01:34:58 +0900

    Screenshot action.

--- actions/page/page.go
+++ actions/page/page.go
@@ -1,8 +1,15 @@
 package page
 
 import (
+	"bytes"
 	"github.com/4ydx/cdp/protocol/page"
 	"github.com/4ydx/chrome-protocol"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"log"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -17,9 +24,61 @@ func GetNavigationEvents() []cdp.Event {
 
 // Navigate sends the browser to the given URL
 func Navigate(frame *cdp.Frame, url string, timeout time.Duration) error {
-	return cdp.NewAction(frame,
+	err := cdp.NewAction(frame,
 		GetNavigationEvents(),
 		[]cdp.Step{
 			cdp.Step{ID: frame.RequestID.GetNext(), Method: page.CommandPageNavigate, Params: &page.NavigateArgs{URL: url}, Reply: &page.NavigateReply{}, Timeout: timeout},
 		}).Run()
+	if err != nil {
+		log.Print(err)
+	}
+	return err
+}
+
+// Screenshot captures a screenshot and saves it to the given destination.
+func Screenshot(frame *cdp.Frame, destination, format string, quality int, clip *page.Viewport, timeout time.Duration) error {
+	var action *cdp.Action
+	if clip != nil {
+		action = cdp.NewAction(frame,
+			[]cdp.Event{},
+			[]cdp.Step{
+				cdp.Step{ID: frame.RequestID.GetNext(), Method: page.CommandPageCaptureScreenshot, Params: &page.CaptureScreenshotArgs{Format: format, Clip: clip, Quality: quality}, Reply: &page.CaptureScreenshotReply{}, Timeout: timeout},
+			})
+	} else {
+		action = cdp.NewAction(frame,
+			[]cdp.Event{},
+			[]cdp.Step{
+				cdp.Step{ID: frame.RequestID.GetNext(), Method: page.CommandPageCaptureScreenshot, Params: &page.CaptureScreenshotArgs{Format: format, Quality: quality}, Reply: &page.CaptureScreenshotReply{}, Timeout: timeout},
+			})
+	}
+	if err := action.Run(); err != nil {
+		log.Print(err)
+		return err
+	}
+
+	// Convert to an image.
+	src := action.Steps[0].Reply.(*page.CaptureScreenshotReply).Data
+	m, _, err := image.Decode(bytes.NewReader(src))
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	// Save to destination.
+	if !strings.HasSuffix(destination, "."+format) {
+		destination = destination + "." + format
+	}
+	f, err := os.Create(destination)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	defer f.Close()
+
+	if format == "png" {
+		png.Encode(f, m)
+	} else {
+		jpeg.Encode(f, m, nil)
+	}
+	return nil
 }
