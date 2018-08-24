7e379ff00e0aa54347750f543f2c1d6776e48009
4ydx <nathan@4ydx.com>
2018-08-24 17:57:22 +0900

    Runtime example of executing a javascript expression and receiving the result.

---
+++ actions/runtime/runtime.go
@@ -0,0 +1,21 @@
+package runtime
+
+import (
+	"github.com/4ydx/cdp/protocol/runtime"
+	"github.com/4ydx/chrome-protocol"
+	"log"
+	"time"
+)
+
+func Evaluate(frame *cdp.Frame, expression string, timeout time.Duration) (*runtime.EvaluateReply, error) {
+	action := cdp.NewAction(frame, []cdp.Event{},
+		[]cdp.Step{
+			cdp.Step{ID: frame.RequestID.GetNext(), Method: runtime.CommandRuntimeEvaluate, Params: &runtime.EvaluateArgs{Expression: expression, Silent: false}, Reply: &runtime.EvaluateReply{}, Timeout: timeout},
+		})
+	err := action.Run()
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+	return action.Steps[0].Reply.(*runtime.EvaluateReply), nil
+}
---
+++ example/runtime/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"github.com/4ydx/chrome-protocol"
+	"github.com/4ydx/chrome-protocol/actions/dom"
+	"github.com/4ydx/chrome-protocol/actions/enable"
+	"github.com/4ydx/chrome-protocol/actions/input"
+	"github.com/4ydx/chrome-protocol/actions/page"
+	"github.com/4ydx/chrome-protocol/actions/runtime"
+	"log"
+	"time"
+)
+
+func main() {
+	frame := cdp.Start(9222)
+	defer cdp.Stop()
+
+	// Enable page and dom events
+	if err := enable.Page(frame, time.Second*2); err != nil {
+		panic(err)
+	}
+	if err := enable.Dom(frame, time.Second*2); err != nil {
+		panic(err)
+	}
+	if err := enable.Runtime(frame, time.Second*2); err != nil {
+		panic(err)
+	}
+
+	// Navigate
+	if err := page.Navigate(frame, "https://google.com", time.Second*5); err != nil {
+		panic(err)
+	}
+
+	// Focus
+	if err := dom.Focus(frame, "#lst-ib", time.Second*5); err != nil {
+		panic(err)
+	}
+
+	// Fill
+	if err := input.Fill(frame, "testing", time.Second*5); err != nil {
+		panic(err)
+	}
+
+	// Determine if the input has the value "testing"
+	reply, err := runtime.Evaluate(frame, "document.getElementById('lst-ib').value.toString()", time.Second*5)
+	if err != nil {
+		panic(err)
+	}
+
+	// We expect that the value of the element will be "testing".  I am seeing a value of &"testing" and I'm not familiar
+	// enough to know why this is prepended by an ampersand, but it is there.  The actual data in the byte array is only
+	// the values "testing".
+	fmt.Printf("%s\n", reply.Result.Value)
+
+	log.Printf("\n-- All completed for %s --\n", frame.FrameID)
+}
